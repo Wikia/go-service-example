db810fd0febd308d182852ee6580baa5b011a944
Łukasz Harasimowicz <dev@harnash.eu>
2019-10-25 11:59:24 +0200

    Fix null pointer exception

--- cmd/example_app/internal/handlers/routes.go
+++ cmd/example_app/internal/handlers/routes.go
@@ -5,15 +5,14 @@ import (
 	"os"
 
 	"github.com/go-chi/chi"
-	"go.uber.org/zap"
 	logmiddleware "github.com/harnash/go-middlewares/logger"
+	"go.uber.org/zap"
 )
 
 // API constructs an http.Handler with all application routes defined.
 func API(shutdown chan os.Signal, logger *zap.SugaredLogger) http.Handler {
-
 	r := chi.NewRouter()
-	r.With(
+	r.Use(
 		logmiddleware.InContext(
 			logmiddleware.WithLogger(func() (*zap.SugaredLogger, error){ return logger, nil })),
 	)
--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/ardanlabs/service v0.0.0-20191017201054-eb87b0e2d3e9 // indirect
 	github.com/caarlos0/env v3.5.0+incompatible
 	github.com/go-chi/chi v4.0.2+incompatible
-	github.com/harnash/go-middlewares v0.0.0-20190703073925-c95f90abe608 // indirect
+	github.com/harnash/go-middlewares v0.0.0-20190703073925-c95f90abe608
 	github.com/pkg/errors v0.8.1
 	go.uber.org/zap v1.9.1
 )
