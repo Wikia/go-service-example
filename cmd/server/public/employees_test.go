acf20570ba196e345c7446392c1eb9077a4a42f1
Łukasz Harasimowicz <dev@harnash.eu>
2021-07-15 22:58:37 +0200

    Added more tests

--- cmd/server/public/employees_test.go
+++ cmd/server/public/employees_test.go
@@ -5,6 +5,8 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"gorm.io/gorm"
+
 	"github.com/Wikia/go-example-service/cmd/models/employee"
 
 	"github.com/Wikia/go-example-service/internal/logging"
@@ -63,5 +65,50 @@ func TestDeleteEmployee(t *testing.T) {
 		assert.Equal(t, 1, mockRepo.DeleteEmployeeCallCount())
 		_, id := mockRepo.DeleteEmployeeArgsForCall(0)
 		assert.EqualValues(t, 1, id)
+	}
+}
+
+func TestFindEmployeeByID(t *testing.T) {
+	t.Parallel()
+	mockRepo := &employeefakes.FakeRepository{}
+	server := public.NewAPIServer(mockRepo)
+
+	mockRepo.GetEmployeeReturns(&stubEmployees[0], nil)
+
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/example/employees/1", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	logging.AddToContext(c, zap.L())
+
+	if assert.NoError(t, server.FindEmployeeByID(c, 1)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, 1, mockRepo.GetEmployeeCallCount())
+		_, id := mockRepo.GetEmployeeArgsForCall(0)
+		assert.EqualValues(t, 1, id)
+		assert.JSONEq(t, `{"ID":1,"Name":"John Wick","City":"Atlanta"}`, rec.Body.String())
+	}
+}
+
+func TestFindEmployeeByIDMissing(t *testing.T) {
+	t.Parallel()
+	mockRepo := &employeefakes.FakeRepository{}
+	server := public.NewAPIServer(mockRepo)
+
+	mockRepo.GetEmployeeReturns(nil, gorm.ErrRecordNotFound)
+
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/example/employees/2", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	logging.AddToContext(c, zap.L())
+
+	err := server.FindEmployeeByID(c, 2)
+	if assert.Error(t, err) {
+		assert.Equal(t, http.StatusNotFound, err.(*echo.HTTPError).Code)
+		assert.Equal(t, 1, mockRepo.GetEmployeeCallCount())
+		_, id := mockRepo.GetEmployeeArgsForCall(0)
+		assert.EqualValues(t, 2, id)
+		assert.Empty(t, rec.Body.String())
 	}
 }
