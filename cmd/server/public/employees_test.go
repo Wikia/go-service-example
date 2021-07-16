9b91300f0eedea6698e0e38b9366381dce82df27
Łukasz Harasimowicz <dev@harnash.eu>
2021-07-16 23:01:00 +0200

    Added more tests and some refactoring

--- cmd/server/public/employees_test.go
+++ cmd/server/public/employees_test.go
@@ -7,15 +7,14 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/pkg/errors"
+	"github.com/opentracing/opentracing-go"
 
-	"github.com/Wikia/go-example-service/internal/validator"
+	"github.com/pkg/errors"
 
 	"gorm.io/gorm"
 
 	"github.com/Wikia/go-example-service/cmd/models/employee"
 
-	"github.com/Wikia/go-example-service/internal/logging"
 	"go.uber.org/zap"
 
 	"github.com/labstack/echo/v4"
@@ -36,151 +35,131 @@ var stubEmployees = []employee.Employee{
 }
 
 func TestGetAllEmployees(t *testing.T) {
-	t.Parallel()
 	mockRepo := &employeefakes.FakeRepository{}
-	server := public.NewAPIServer(mockRepo)
+	e := public.NewPublicAPI(zap.L(), opentracing.NoopTracer{}, "test-server", mockRepo, nil)
 
 	mockRepo.GetAllEmployeesReturns(stubEmployees, nil)
 
-	e := echo.New()
 	req := httptest.NewRequest(http.MethodGet, "/example/employee/all", nil)
 	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
-	logging.AddToContext(c, zap.L())
+	e.ServeHTTP(rec, req)
 
-	if assert.NoError(t, server.GetAllEmployees(c)) {
-		assert.Equal(t, http.StatusOK, rec.Code)
-		assert.Equal(t, 1, mockRepo.GetAllEmployeesCallCount())
-		assert.JSONEq(t, `[{"ID":1,"Name":"John Wick","City":"Atlanta"},{"ID":2,"Name":"Wade Winston Wilson","City":"New York"}]`, rec.Body.String())
-	}
+	assert.Equal(t, http.StatusOK, rec.Code)
+	assert.Equal(t, 1, mockRepo.GetAllEmployeesCallCount())
+	assert.JSONEq(t, `[{"ID":1,"Name":"John Wick","City":"Atlanta"},{"ID":2,"Name":"Wade Winston Wilson","City":"New York"}]`, rec.Body.String())
 }
 
 func TestGetAllEmployeesFail(t *testing.T) {
-	t.Parallel()
 	mockRepo := &employeefakes.FakeRepository{}
-	server := public.NewAPIServer(mockRepo)
+	e := public.NewPublicAPI(zap.L(), opentracing.NoopTracer{}, "test-server", mockRepo, nil)
 
 	mockRepo.GetAllEmployeesReturns(nil, errors.New("some error"))
 
-	e := echo.New()
 	req := httptest.NewRequest(http.MethodGet, "/example/employee/all", nil)
 	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
-	logging.AddToContext(c, zap.L())
+	e.ServeHTTP(rec, req)
 
-	err := server.GetAllEmployees(c)
-	if assert.Error(t, err) {
-		assert.Equal(t, http.StatusInternalServerError, err.(*echo.HTTPError).Code)
-		assert.Equal(t, 1, mockRepo.GetAllEmployeesCallCount())
-	}
+	assert.Equal(t, http.StatusInternalServerError, rec.Code)
+	assert.Equal(t, 1, mockRepo.GetAllEmployeesCallCount())
 }
 
 func TestDeleteEmployee(t *testing.T) {
-	t.Parallel()
 	mockRepo := &employeefakes.FakeRepository{}
-	server := public.NewAPIServer(mockRepo)
+	e := public.NewPublicAPI(zap.L(), opentracing.NoopTracer{}, "test-server", mockRepo, nil)
 
-	e := echo.New()
 	req := httptest.NewRequest(http.MethodDelete, "/example/employee/1", nil)
 	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
-	logging.AddToContext(c, zap.L())
+	e.ServeHTTP(rec, req)
 
-	if assert.NoError(t, server.DeleteEmployee(c, 1)) {
-		assert.Equal(t, http.StatusAccepted, rec.Code)
-		assert.Equal(t, 1, mockRepo.DeleteEmployeeCallCount())
-		_, id := mockRepo.DeleteEmployeeArgsForCall(0)
-		assert.EqualValues(t, 1, id)
-	}
+	assert.Equal(t, http.StatusAccepted, rec.Code)
+	assert.Equal(t, 1, mockRepo.DeleteEmployeeCallCount())
+	_, id := mockRepo.DeleteEmployeeArgsForCall(0)
+	assert.EqualValues(t, 1, id)
 }
 
 func TestDeleteEmployeeMissing(t *testing.T) {
-	t.Parallel()
 	mockRepo := &employeefakes.FakeRepository{}
-	server := public.NewAPIServer(mockRepo)
+	e := public.NewPublicAPI(zap.L(), opentracing.NoopTracer{}, "test-server", mockRepo, nil)
+
 	mockRepo.DeleteEmployeeReturns(gorm.ErrRecordNotFound)
 
-	e := echo.New()
 	req := httptest.NewRequest(http.MethodDelete, "/example/employee/5", nil)
 	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
-	logging.AddToContext(c, zap.L())
+	e.ServeHTTP(rec, req)
 
-	err := server.DeleteEmployee(c, 5)
-	if assert.Error(t, err) {
-		assert.Equal(t, http.StatusNotFound, err.(*echo.HTTPError).Code)
-		assert.Equal(t, 1, mockRepo.DeleteEmployeeCallCount())
-		_, id := mockRepo.DeleteEmployeeArgsForCall(0)
-		assert.EqualValues(t, 5, id)
-	}
+	assert.Equal(t, http.StatusNotFound, rec.Code)
+	assert.Equal(t, 1, mockRepo.DeleteEmployeeCallCount())
+	_, id := mockRepo.DeleteEmployeeArgsForCall(0)
+	assert.EqualValues(t, 5, id)
 }
 
 func TestFindEmployeeByID(t *testing.T) {
-	t.Parallel()
 	mockRepo := &employeefakes.FakeRepository{}
-	server := public.NewAPIServer(mockRepo)
+	e := public.NewPublicAPI(zap.L(), opentracing.NoopTracer{}, "test-server", mockRepo, nil)
 
 	mockRepo.GetEmployeeReturns(&stubEmployees[0], nil)
 
-	e := echo.New()
 	req := httptest.NewRequest(http.MethodGet, "/example/employee/1", nil)
 	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
-	logging.AddToContext(c, zap.L())
+	e.ServeHTTP(rec, req)
 
-	if assert.NoError(t, server.FindEmployeeByID(c, 1)) {
-		assert.Equal(t, http.StatusOK, rec.Code)
-		assert.Equal(t, 1, mockRepo.GetEmployeeCallCount())
-		_, id := mockRepo.GetEmployeeArgsForCall(0)
-		assert.EqualValues(t, 1, id)
-		assert.JSONEq(t, `{"ID":1,"Name":"John Wick","City":"Atlanta"}`, rec.Body.String())
-	}
+	assert.Equal(t, http.StatusOK, rec.Code)
+	assert.Equal(t, 1, mockRepo.GetEmployeeCallCount())
+	_, id := mockRepo.GetEmployeeArgsForCall(0)
+	assert.EqualValues(t, 1, id)
+	assert.JSONEq(t, `{"ID":1,"Name":"John Wick","City":"Atlanta"}`, rec.Body.String())
 }
 
 func TestFindEmployeeByIDMissing(t *testing.T) {
-	t.Parallel()
 	mockRepo := &employeefakes.FakeRepository{}
-	server := public.NewAPIServer(mockRepo)
+	e := public.NewPublicAPI(zap.L(), opentracing.NoopTracer{}, "test-server", mockRepo, nil)
 
 	mockRepo.GetEmployeeReturns(nil, gorm.ErrRecordNotFound)
 
-	e := echo.New()
 	req := httptest.NewRequest(http.MethodGet, "/example/employee/2", nil)
 	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
-	logging.AddToContext(c, zap.L())
 
-	err := server.FindEmployeeByID(c, 2)
-	if assert.Error(t, err) {
-		assert.Equal(t, http.StatusNotFound, err.(*echo.HTTPError).Code)
-		assert.Equal(t, 1, mockRepo.GetEmployeeCallCount())
-		_, id := mockRepo.GetEmployeeArgsForCall(0)
-		assert.EqualValues(t, 2, id)
-		assert.Empty(t, rec.Body.String())
-	}
+	e.ServeHTTP(rec, req)
+	assert.Equal(t, http.StatusNotFound, rec.Code)
+	assert.Equal(t, 1, mockRepo.GetEmployeeCallCount())
+	_, id := mockRepo.GetEmployeeArgsForCall(0)
+	assert.EqualValues(t, 2, id)
+	assert.JSONEq(t, `{"message":"object with given id not found"}`, rec.Body.String())
 }
 
 func TestCreateEmployee(t *testing.T) {
-	t.Parallel()
 	mockRepo := &employeefakes.FakeRepository{}
-	server := public.NewAPIServer(mockRepo)
+	e := public.NewPublicAPI(zap.L(), opentracing.NoopTracer{}, "test-server", mockRepo, nil)
 
-	e := echo.New()
-	e.Validator = &validator.EchoValidator{}
 	payload, err := json.Marshal(stubEmployees[0])
 	assert.NoError(t, err)
 
-	req := httptest.NewRequest(http.MethodPut, "/example/employee/", bytes.NewBuffer(payload))
+	req := httptest.NewRequest(http.MethodPut, "/example/employee", bytes.NewBuffer(payload))
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
-	logging.AddToContext(c, zap.L())
+	e.ServeHTTP(rec, req)
 
-	if assert.NoError(t, server.CreateEmployee(c)) {
-		assert.Equal(t, http.StatusCreated, rec.Code)
-		assert.Equal(t, 1, mockRepo.AddEmployeeCallCount())
-		_, ret := mockRepo.AddEmployeeArgsForCall(0)
-		assert.EqualValues(t, &stubEmployees[0], ret)
-		assert.Empty(t, rec.Body.String())
-	}
+	assert.Equal(t, http.StatusCreated, rec.Code)
+	assert.Equal(t, 1, mockRepo.AddEmployeeCallCount())
+	_, ret := mockRepo.AddEmployeeArgsForCall(0)
+	assert.EqualValues(t, &stubEmployees[0], ret)
+	assert.Empty(t, rec.Body.String())
+}
+
+func TestCreateEmployeeInvalid(t *testing.T) {
+	mockRepo := &employeefakes.FakeRepository{}
+	e := public.NewPublicAPI(zap.L(), opentracing.NoopTracer{}, "test-server", mockRepo, nil)
+
+	badEmployee := employee.Employee{Name: "Joker"}
+	payload, err := json.Marshal(badEmployee)
+	assert.NoError(t, err)
+
+	req := httptest.NewRequest(http.MethodPut, "/example/employee", bytes.NewBuffer(payload))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+
+	e.ServeHTTP(rec, req)
+	assert.Equal(t, http.StatusBadRequest, rec.Code)
+	assert.Regexp(t, `Error:Field validation`, rec.Body.String())
+	assert.Equal(t, 0, mockRepo.AddEmployeeCallCount())
 }
